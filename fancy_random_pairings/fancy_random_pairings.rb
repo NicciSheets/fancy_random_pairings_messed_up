9da0031eb7a48ca21f12f16919b3fc2fb37503a9
NicciSheets <39924081+NicciSheets@users.noreply.github.com>
2018-07-30 00:28:28 -0400

    Add files via upload

---
+++ fancy_random_pairings/fancy_random_pairings.rb
@@ -0,0 +1,20 @@
+def name_arr(names)
+	names.shuffle
+end 
+
+def name_arr_paired(names)
+	name_arr(names).each_slice(2).to_a
+end
+
+def pairings(names)
+	pairs = name_arr_paired(names)
+	if names.length % 2 == 0 
+		pairs
+	else 
+		pairs[-2].concat(pairs[-1]).flatten
+		pairs.pop(1) 
+	end
+	pairs
+end
+#names = ["1", "2", "3", "4", "5"]
+#puts name_arr(names)
